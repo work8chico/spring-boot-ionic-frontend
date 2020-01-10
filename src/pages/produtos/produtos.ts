1469fef4ddd10d98935280f193a6d2bc183bd3f7
FRANCISCO DE ASSIS DA SILVA JUNIOR <francisco.a.silva@fiesc.com.br>
2020-01-10 11:48:09 -0300

    Carregando produtos de uma dada categoria

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
 @Component({
@@ -11,10 +12,22 @@ export class ProdutosPage {
 
 items : ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoSerice: ProdutoService) {
   }
 
   ionViewDidLoad() {
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoSerice.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+      },
+      error => {});
+      
+
+
     this.items = [
       {
         id: "1",
