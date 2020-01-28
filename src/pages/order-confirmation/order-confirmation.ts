02144fa12564f1efc41843ea9c8516c3aa327b19
FRANCISCO DE ASSIS DA SILVA JUNIOR <francisco.a.silva@fiesc.com.br>
2020-01-28 17:03:30 -0300

    Informando pedido registrado ao cliente

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido : string;
 
   constructor(
     public navCtrl: NavController,
@@ -60,11 +61,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
-      this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.cartService.createOrClearCart();      
+      this.codpedido = this.extractId(response.headers.get('location'));
     },
     error =>{
       if(error.status == 403){
@@ -72,6 +77,11 @@ export class OrderConfirmationPage {
       }
     });
 
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
